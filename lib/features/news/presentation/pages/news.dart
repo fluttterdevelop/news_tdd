75361369d92e70013e5d6270f7e083cd239ba000
muhammadamin <mubashshirahmad999@gmail.com>
2023-12-27 04:51:59 +0500

    UI QISMI HAM QO'SHILDI

---
+++ lib/features/news/presentation/pages/news.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_tdd/features/news/data/model/news_article/news_article.dart';
+import 'package:news_tdd/features/news/presentation/bloc/news/news_bloc.dart';
+
+class NewsScreen extends StatelessWidget {
+  final NewsArticleModel article;
+
+  const NewsScreen({
+    super.key,
+    required this.article,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => NewsBloc(),
+      child: Builder(builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: BlocBuilder<NewsBloc, NewsState>(
+              builder: (context, state) {
+                return Text("News ${state.news}");
+              },
+            ),
+          ),
+          body: ListView(
+            children: [
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  Navigator.of(context).pushNamed('/news_single');
+                },
+                child: Image.network(
+                  article.urlToImage, // Use the article's urlToImage property
+                  width: double.maxFinite,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Text(article
+                  .description), // Use the article's description property
+              Text(article.contant), // Use the article's content property
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
